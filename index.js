bfd66d07fa3abf8c944497898052eb565fe17a1a
delbozkester <delbozkester@gmail.com>
2019-10-10 17:34:21 -0500

    Change in order

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 //Imports
 var convert = require('./lib/convert');
 
-colorine.rgb_to_lab  = convert.rgb_to_lab;
-colorine.rgba_to_lab = convert.rgba_to_lab;
-
 //Exports
 var colorine = module.exports = {};
+
+colorine.rgb_to_lab  = convert.rgb_to_lab;
+colorine.rgba_to_lab = convert.rgba_to_lab;
